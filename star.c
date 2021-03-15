93dc7bc3a38ed8de95f56056aa4294262f3177b2
nared456 <78350512+nared456@users.noreply.github.com>
2021-03-15 19:45:35 +0700

    6320503041

--- star.c
+++ star.c
@@ -1,21 +1,43 @@
 #include<stdio.h>
 int main()
 {
-    long long student,i,j,k=0;
+    int student,i,j,k,min=0,max=0,No1,No2;
     scanf("%d",&student);
-    int people_vote[student][2];
+    int people_vote_x[student];
+    int people_vote_y[student];
     int check[student];
     for(i=0;i<student;i++)
     {
-        for(j=0;j<2;j++)
+        scanf("%d %d", &people_vote_x[i], &people_vote_y[i]);  
+    }
+    for(i=1;i<=student;i++)
+    {
+        check[i-1] = 0;
+        for(j=0;j<student;j++)
         {
-            scanf("%d", &people_vote[i][j]);
+            if(i==people_vote_x[j])
+            {
+                check[i-1]++;
+            }
+            else if(j==people_vote_y[j])
+            {
+                check[i-1]++;
+            }
         }
     }
-    check[0] = 1;
-    for(i=0;i<student;i++)
+    min = check[0];
+    for(k = 0; i<student; i++)
     {
-        check[i+1] = check[i] + 1; 
+        if(check[k] > max)
+        {
+            max = check[k];
+            No1 = k+1;
+        }
+        if(check[k] < min)
+        {
+            min = check[k];
+            No2 = k+1;
+        }
     }
-    
+    printf("%d %d",No1,No2);
 }
